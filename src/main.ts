7b89fdc575dbfd38993141306ec0e492c52769c2
yingwhy <wangxy.hb@foxmail.com>
2022-02-25 16:35:05 +0800

    style-class

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { components, plugins } from '@/plugins/elplus'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+components.forEach(component => {
+  app.component(component.name, component)
+})
+plugins.forEach(plugin => {
+  app.use(plugin)
+})
+app.use(store).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    123
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Options, Vue } from 'vue-class-component'
+import HelloWorld from '@/components/HelloWorld.vue'
 
 @Options({
   components: {
